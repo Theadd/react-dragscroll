fe1433ce67540f4d3ffd0e6595395131f4f60ffc
Theadd <pantallazo@gmail.com>
2016-12-02 02:30:27 +0100

    .gitignore, scripts in package.json, DragScroll style prop, etc.

--- src/DragScroll.jsx
+++ src/DragScroll.jsx
@@ -5,45 +5,60 @@
 import React from "react";
 
 export default class DragScroll extends React.Component {
-  constructor(props) {
+
+  constructor (props) {
     super(props);
+
     this.state = {
       data: props.dataSource,
       dragging: false
     };
   }
 
-  render() {
-    let sytle = null;
-    if (this.props.height && this.props.width) {
-      sytle = {style: {height: this.props.height, width: this.props.width, overflow: 'auto'}};
-    }
-    return <div className={this.props.className} {...sytle}
-                onMouseUp={this.mouseUpHandle.bind(this)}
-                onMouseMove={this.mouseMoveHandle.bind(this)}
-                ref="container">
-      {this.renderChildren(this.props.children)}
-    </div>;
+  render () {
+    const {
+      children,
+      width = 'auto',
+      height = 'auto',
+      style = {},
+      className = ''
+    } = this.props
+
+    const divStyles = Object.assign({
+      overflow: 'auto',
+      width,
+      height
+    }, style)
+
+    return (
+      <div className={ className }
+           style={ divStyles }
+           onMouseUp={ this.mouseUpHandle.bind(this) }
+           onMouseMove={ this.mouseMoveHandle.bind(this) }
+           ref="container">
+        { this.renderChildren(children) }
+      </div>
+    )
   }
 
-  componentDidMount() {
+  componentDidMount () {
     window.addEventListener('mouseup', this.mouseUpHandle.bind(this));
     window.addEventListener('mousemove', this.mouseMoveHandle.bind(this));
   }
 
-  componentWillUnmount() {
+  componentWillUnmount () {
     window.removeEventListener('mouseup', this.mouseUpHandle.bind(this));
     window.removeEventListener('mousemove', this.mouseMoveHandle.bind(this));
   }
 
-  mouseUpHandle(e) {
+  mouseUpHandle (e) {
     if (this.state.dragging) {
       this.state.dragging = false;
       this.setState(this.state);
     }
   }
 
-  mouseDownHandle(e) {
+  mouseDownHandle (e) {
     if (!this.state.dragging) {
       this.state.dragging = true;
       this.setState(this.state);
@@ -53,37 +68,41 @@ export default class DragScroll extends React.Component {
     }
   }
 
-  mouseMoveHandle(e) {
+  mouseMoveHandle (e) {
     if (this.state.dragging) {
-      this.refs.container.scrollLeft -=
-        (-this.lastClientX + (this.lastClientX = e.clientX));
-      this.refs.container.scrollTop -=
-        (-this.lastClientY + (this.lastClientY = e.clientY));
+      this.refs.container.scrollLeft -= (-this.lastClientX + (this.lastClientX = e.clientX));
+      this.refs.container.scrollTop -= (-this.lastClientY + (this.lastClientY = e.clientY));
     }
   }
 
-  renderChildren(dom, type) {
+  renderChildren (dom, type) {
     if (this.isArray(dom)) {
       return dom.map((item, index) => {
         return React.cloneElement(item, {
           key: item.key || index,
-          onMouseUp: this.mouseUpHandle.bind(this),
-          onMouseDown: this.mouseDownHandle.bind(this)
+          onMouseUp: this
+            .mouseUpHandle
+            .bind(this),
+          onMouseDown: this
+            .mouseDownHandle
+            .bind(this)
         });
       });
     } else if ('object' == typeof dom) {
       return React.cloneElement(dom, {
-        onMouseUp: this.mouseUpHandle.bind(this),
-        onMouseDown: this.mouseDownHandle.bind(this)
+        onMouseUp: this
+          .mouseUpHandle
+          .bind(this),
+        onMouseDown: this
+          .mouseDownHandle
+          .bind(this)
       });
     }
   }
 
-  isArray(object){
-    return  object && typeof object==='object' &&
-      typeof object.length==='number' &&
-      typeof object.splice==='function' &&
-      //判断length属性是否是可枚举的 对于数组 将得到false
-      !(object.propertyIsEnumerable('length'));
+  isArray (object) {
+    return object && typeof object === 'object' && typeof object.length === 'number' && typeof object.splice === 'function' &&
+    //判断length属性是否是可枚举的 对于数组 将得到false
+    !(object.propertyIsEnumerable('length'));
   }
 }
